23d258f505cea0cd47d7dc1ea7410ae802b3708f
jangwooooo <s22018@gsm.hs.kr>
2022-12-10 18:32:01 +0900

    PostMapping -> DeleteMapping으로 변경

--- src/main/java/example/crud/controller/BoardController.java
+++ src/main/java/example/crud/controller/BoardController.java
@@ -4,6 +4,7 @@ import example.crud.entity.BoardDto;
 import example.crud.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ public class BoardController {
         return "test";
     }
 
-    @PostMapping("/board/delete/{id}")
+    @DeleteMapping("/board/delete/{id}")
     public String boardDelete(@PathVariable Long id) {
         boardService.delete(id);
         return "test";
